8ad96e09163755bd08ff83a53eb0a82372c1c555
Ha-limLee <leehalim09@gmail.com>
2021-05-12 17:04:15 +0900

    implemented note add and delete

--- app/renderer/components/lobby/NoteIndex.js
+++ app/renderer/components/lobby/NoteIndex.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
 export default class NoteIndex extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      id: this.props.id
+    }
+  }
+
   render () {
+    const deleteThis = () => {
+      this.props.deleteCallBack(this.state.id)
+    }
+
     return (
-      <div className='note_index'>
+      <div className='note_index' onClick={console.log(this.state.id)}>
+        <button onClick={deleteThis}>
+          제거
+        </button>
         <NoteFrame
           summary='Lorem ipsum'
         />
@@ -18,13 +32,17 @@ class NoteFrame extends Component {
   }
 
   render () {
+    const id = new Date().toTimeString().slice(0, 8)
+
     const doClick = e => this.handleClick(e)
     return (
       <div
         className='note_frame'
         onClick={doClick}
       >
+        
         <p className='contents summary'>{this.props.summary}</p>
+        <textarea placeholder="click here"></textarea>
       </div>
     )
   }
