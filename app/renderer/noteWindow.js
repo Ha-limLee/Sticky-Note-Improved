560dfd76a84d4cda187d469343edee6abf745d77
Park, Jonghyeon <belline0124@gmail.com>
2021-05-17 14:34:27 +0900

    Add NoteWindow

--- app/renderer/noteWindow.js
+++ app/renderer/noteWindow.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/commons/Header'
-import NoteIndex from './components/lobby/NoteIndex'
+import Editor from './components/noteWindow/Editor'
 
 export default class App extends Component {
+  constructor (props) {
+    super()
+  }
+
+  setTitle (title) {
+    document.title = title
+  }
+
   render () {
+    const onChangeHandler = () => {
+      this.setTitle('Sticky Note')
+    }
     return (
       <div>
         <Header
           title='안녕하세요.'
+          editable='true'
+          onChange={onChangeHandler}
         />
-        <NoteIndex />
+        <Editor />
       </div>
     )
   }
--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const electron = require('electron')
-const path = require('path')
-const ipcMain = electron.ipcMain
 const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const { nanoid } = require('nanoid')
+const { createWindow } = require('./app/main/mainWindow')
 
 let mainWindow
 app.on('ready', createWindow)
@@ -11,38 +8,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 app.on('activate', () => {
-  if (mainWindow == null) createWindow()
+  if (mainWindow == null) createWindow(mainWindow)
 })
 
-ipcMain.on('NoteFrameClicked', (event, arg) => {
-  /* noteWindow = new BrowserWindow({
-    width: 400,
-    height: 500,
-    webPreferences: {}
-  }) */
-})
-
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 400,
-    height: 500,
-    webPreferences: {
-      preload: path.join(__dirname, 'app', 'renderer', 'preloads', 'index.js'),
-      nodeIntegration: true,
-      contextIsolation: true,
-      enableRemoteModule: false
-    }
-  })
-  mainWindow.loadURL('file://' + path.join(__dirname, 'app', 'windows', 'index.html'))
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-}
-
-// back-end logic
-
-ipcMain.handle('nanoid-ipc', async (event) => {
-  const id = nanoid()
-  console.log(id)
-  return id
-})
+require('./app/main/ipcMain')
