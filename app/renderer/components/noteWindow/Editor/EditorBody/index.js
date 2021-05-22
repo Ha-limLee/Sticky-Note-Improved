97f351ee9b6220779c8e66ca81bc47a014c22f2d
Ha-limLee <leehalim09@gmail.com>
2021-05-22 19:23:09 +0900

    implemented Editor/EditorBody placeholder

--- app/renderer/components/noteWindow/Editor/EditorBody/index.js
+++ app/renderer/components/noteWindow/Editor/EditorBody/index.js
@@ -3,15 +3,48 @@ import React, { Component } from 'react'
 export default class EditorBody extends Component {
   constructor (props) {
     super()
+    this.state = {
+      text: ''
+    }
+    this.textInput = React.createRef()
+    this.getText = this.getText.bind(this)
   }
 
-  // Todo: Content Editable 엘리먼트의 Place Holder 설정 방법?
+  getText () {
+    return {
+      key: this.props.id,
+      value: this.textInput.current.innerHTML
+    }
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentDidUpdate () {
+    console.log(this.state.text)
+  }
+
+  handleChange (event) {
+    this.setState({
+      text: event.currentTarget.textContent
+    })
+  }
+
   render () {
+    const getText = this.getText
+    const handleChange = this.handleChange.bind(this)
+
     return (
-      <div className='editor_body' contentEditable>
-        <div className='placeHolder'>
-          <p>안녕하세요, Sticky Note입니다. 이 문장을 지우고 메모를 시작하세요.</p>
-        </div>
+      <div className='editor_body'>
+        <span
+          className='textarea'
+          role='textbox'
+          value={this.state.text}
+          onBlur={handleChange}
+          contentEditable
+        />
+        <button onClick={getText}>log</button>
       </div>
     )
   }
